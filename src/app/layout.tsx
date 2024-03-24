63431046e591391b95f24cb583d3c931a5f17ac0
Landry Carroll <lcarroll@fairwinds.org>
2024-03-24 10:14:53 -0400

    fix: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,41 +7,7 @@ import { siteConfig } from '@/config/site'
 import { Providers } from '@/components/providers'
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  metadataBase: new URL(siteConfig.url),
-  description: siteConfig.description,
-  keywords: [],
-  creator: 'landrycarroll',
-  openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-  },
-  icons: {
-    icon: '/favicon.ico',
-    // shortcut: '/favicon-16x16.png',
-    // apple: '/apple-touch-icon.png',
-  },
+  title: 'Notes',
 }
 
 export default function RootLayout({
