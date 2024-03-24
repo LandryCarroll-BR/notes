9f6484375ade20cf9874a63db3faada25d716867
Landry Carroll <lcarroll@fairwinds.org>
2024-03-24 09:47:12 -0400

    feat: add basic welcome text to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import { Dashboard } from '@/components/dashboard'
+import { Box, Flex } from '@/components/layout'
+import { Heading, Lead } from '@/components/typography'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default async function Home() {
-  return <div>HOME</div>
+  return (
+    <Flex className="min-h-screen items-center justify-center pb-48">
+      <Flex className="mx-auto flex-col items-center justify-center gap-4 text-center">
+        <Heading as="h1">Welcome to Notes</Heading>
+        <Lead>A helpful notetaking app with AI functionality</Lead>
+        <Button asChild className="w-fit">
+          <Link href={'/dashboard'}>Start taking notes</Link>
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
